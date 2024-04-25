328d7e4ce59be89a18fc0aeca040405aea3fccfa
rishiraj1212 <rishiraj4878@gmail.com>
2024-04-25 18:29:39 +0530

    update readmi

---
+++ single_inheritance.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+class Rishi{
+    public:
+    void name1(){
+    cout<<"My name is Rishi Raj";
+    }
+
+};
+class Favroit   Rishi{
+    public:
+    void name2(){
+        cout<<"My favroit fruit is Mango";
+    }
+};
+int main(){
+    Favroit p;
+    cout<<"p.name1"<<endl;
+    cout<<"p.name2"<<endl;
+    return 0;
+}
